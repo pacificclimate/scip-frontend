ddbcbadabbf1cd65d6bac1fb7220ed78e41d680e
Lee Zeman <lzeman@uvic.ca>
2022-09-16 11:28:20 -0700

    update map

--- src/components/DataMap/DataMap.js
+++ src/components/DataMap/DataMap.js
@@ -41,14 +41,14 @@ function DataMap({regionBoundary, watershedStreams, downstream}) {
           <SimpleGeoJSON data={watershedFeature} fill={false} color="#6699FF"/>
           <SimpleGeoJSON data={downstreamFeature} fill={false} color="#6699FF"/>
           <WMSTileLayer
-            url={"https://services.pacificclimate.org/ncwms"}
+            url={"https://services.pacificclimate.org/dev/ncwms"}
             format={'image/png'}
             noWrap={true}
             opacity={0.3}
             transparent={true}
             version={'1.1.1'}
-            layers={"x/storage/data/projects/comp_support/climate_explorer_data_prep/climatological_means/pcic12/tasmax_aClimMean_BCCAQv2_PCIC12_historical+rcp85_rXi1p1_19610101-19901231_Canada.nc/tasmax"}
-            time={"1977-07-02T00:00:00Z"}
+            layers={"x/storage/data/projects/comp_support/bc-srif/climatologies/fraser/annual/means/apf_flow_aClimMean_BCCAQv2_CanESM2_historical-rcp85_r1i1p1_19710101-20001231_fraser.nc/apf_flow"}
+            time={"1985-07-02T00:00:00Z"}
             styles={"default-scalar/x-Occam"}
           />
         </BCBaseMap>
