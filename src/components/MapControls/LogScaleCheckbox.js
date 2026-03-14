ea2f64e85aa0e21eadcb437bf39d757494fe5d3e
Lee Zeman <lzeman@uvic.ca>
2026-03-13 20:02:00 -0700

    tidy up code related to data errors that have been fixed

--- src/components/MapControls/LogScaleCheckbox.js
+++ src/components/MapControls/LogScaleCheckbox.js
@@ -11,18 +11,9 @@ function LogScaleCheckbox({mapDataset, handleChange}) {
     // logscaled colour is allowed (can be turned on by user) for only those
     // variables that say so in the configuration file. Additionally, the minimum
     // value of the data range must be > 0
-    
-    // at present, due to a data bug, the files that are supposed to always have
-    // data greater than zero (stream flow magnitude files) have some zero values, 
-    // perhaps due to a rounding error. This causes ncWMS to throw an error when
-    // asked to display them with a logarithmic colour scale, so this function is
-    // disabled for now.
-    
-    //TODO: uncomment this function when data issue is fixed.
     function allowLogscale() {
         return indicatorOptions?.[mapDataset.variable]?.logscale;
     }
-    //function allowLogscale() {return false};
 
     const indicatorOptions = useStore((state) => state.indicatorOptions);
     const min = indicatorOptions?.[mapDataset.variable] ?
