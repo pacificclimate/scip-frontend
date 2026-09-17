0892542ce8e2d8469179076de1565c83c25382e1
Quintin <quintins@uvic.ca>
2026-09-17 10:37:45 -0700

    Tune viewport

--- src/components/DataMap/DataMap.js
+++ src/components/DataMap/DataMap.js
@@ -1,183 +1,190 @@
-// Displays a leaflet map with an ncWMS data layer. 
+// Displays a leaflet map with an ncWMS data layer.
 // Upon receiving a watershed, displays a data overlay (ncWMS), and geoJSON objects
 // representing the watershed boundary (from watershed object), as well as
 // path to the sea and stream connectivity within the watershed (from PCEX API).
 
-import { BCBaseMap, EEZLayer, SetView } from 'pcic-react-leaflet-components';
-import SimpleGeoJSON from '../SimpleGeoJSON/SimpleGeoJSON.js';
-import { WMSTileLayer, FeatureGroup } from 'react-leaflet';
-import { EditControl } from 'react-leaflet-draw';
-import {useState} from 'react';
-import useStore from '../../store/useStore.js';
-import _ from 'lodash';
+import { BCBaseMap, EEZLayer, SetView } from "pcic-react-leaflet-components";
+import SimpleGeoJSON from "../SimpleGeoJSON/SimpleGeoJSON.js";
+import { WMSTileLayer, FeatureGroup } from "react-leaflet";
+import { EditControl } from "react-leaflet-draw";
+import { useState } from "react";
+import useStore from "../../store/useStore.js";
+import _ from "lodash";
 
 // Keep the BC Albers map within the geographic
-// extent it is intended to show; 
+// extent it is intended to show;
 const mapBounds = [
   [45, -145],
-  [65, -105]
+  [65, -105],
 ];
 
-
-function DataMap({regionBoundary, downstream, onSelectOutlet, selectedOutlet, dataset}) {
+function DataMap({
+  regionBoundary,
+  downstream,
+  onSelectOutlet,
+  selectedOutlet,
+  dataset,
+}) {
   const viewport = BCBaseMap.initialViewport;
   const [cmMap, setCMMap] = useState(null);
   // indicator display options loaded from a config file - used for max and min
   const indicatorOptions = useStore((state) => state.indicatorOptions);
 
-  
   //convert the geoJSON to a Feature so it can be displayed on the map.
-  const boundaryFeature = regionBoundary ? {
-      type: "Feature",
-      geometry: regionBoundary
-  } : null;
-
-  const downstreamFeature = downstream ? {
-      type: "Feature",
-      properties: {test: "test"},
-      geometry: {
-          type: "LineString",
-          coordinates: downstream.boundary.geometry.coordinates
+  const boundaryFeature = regionBoundary
+    ? {
+        type: "Feature",
+        geometry: regionBoundary,
       }
-  } : null;
-  
-  function handleAreaCreated(e) {
-  
-      const lat = e.layer._latlng.lat;
-      const lon = e.layer._latlng.lng;
-      const WKT = `POINT (${lon} ${lat})`;
-      onSelectOutlet(WKT);
+    : null;
 
-      // delete the old circlemarker - we can iterate through the layers
-      // of the map until we find one that has a radius and a latlng, 
-      // which has to be a circlemarker. We can compare the latlong to the
-      // current one to find the "old" circlemarker layer.
-      const map = e.layer._map;
-      
-      // set map reference so we can delete this layer later
-      // if we switch to selcting a region from tha dropdowns
-      setCMMap(map);
-      const layers = map._layers;
-      
-      const oldMarker = _.findKey(layers, l => {
-          return(l._radius && l._latlng?.lat !== lat && l._latlng?.lon !== lon);
-      });
-      
-      if(oldMarker) {
-          map.removeLayer(layers[oldMarker]);
-          }
+  const downstreamFeature = downstream
+    ? {
+        type: "Feature",
+        properties: { test: "test" },
+        geometry: {
+          type: "LineString",
+          coordinates: downstream.boundary.geometry.coordinates,
+        },
+      }
+    : null;
+
+  function handleAreaCreated(e) {
+    const lat = e.layer._latlng.lat;
+    const lon = e.layer._latlng.lng;
+    const WKT = `POINT (${lon} ${lat})`;
+    onSelectOutlet(WKT);
+
+    // delete the old circlemarker - we can iterate through the layers
+    // of the map until we find one that has a radius and a latlng,
+    // which has to be a circlemarker. We can compare the latlong to the
+    // current one to find the "old" circlemarker layer.
+    const map = e.layer._map;
+
+    // set map reference so we can delete this layer later
+    // if we switch to selcting a region from tha dropdowns
+    setCMMap(map);
+    const layers = map._layers;
+
+    const oldMarker = _.findKey(layers, (l) => {
+      return l._radius && l._latlng?.lat !== lat && l._latlng?.lon !== lon;
+    });
+
+    if (oldMarker) {
+      map.removeLayer(layers[oldMarker]);
+    }
   }
-  
+
   function handleAreaDeleted(e) {
-      onSelectOutlet(null);
+    onSelectOutlet(null);
   }
-    
+
   // this cleans up a residual layer containing a circlemarker
   // if the user has placed a circlemarker on the map to select
-  // the area upstream of a point, but then the user switches to 
+  // the area upstream of a point, but then the user switches to
   // selecting regions from the dropdown menus.
   // the triggering of this cleanup is a little bit messy, but I was
   // unable to get more reasonable ways to work.
   // when a circlemarker is placed on the map, the drawing handler
-  // function stores the map in the cmMap state variable and sends a 
-  // callback to the parent component with the latlong of the 
+  // function stores the map in the cmMap state variable and sends a
+  // callback to the parent component with the latlong of the
   // circlemaker, which is eventually received by this component
   // in the selectedOutlet state variable.
   // When the user selects a region from a dropdown, SelectedOutlet
-  // is set to null, because dropdown regions are not selected with 
+  // is set to null, because dropdown regions are not selected with
   // an outlet.
   // this cleanup is triggered by the combination of cmMap being set
   // (ie, the user has ever drawn a circlemarker) and selectedOutlet
-  // being unset (ie, the user is not looking at a circlemarker 
-  // right now). 
-  // it accesses the map via cmMap, finds the layer corresponding to 
+  // being unset (ie, the user is not looking at a circlemarker
+  // right now).
+  // it accesses the map via cmMap, finds the layer corresponding to
   // a circle outlet, and deletes it. Then it unsets cmMap, so that
   // this cleanup is only run once (until the user draws another marker,
   // which will set cmMap again).
   if (cmMap !== null && selectedOutlet == null) {
     const layers = cmMap._layers;
-      
-    const oldMarker = _.findKey(layers, l => {
-      return(l._radius && l._latlng);
+
+    const oldMarker = _.findKey(layers, (l) => {
+      return l._radius && l._latlng;
     });
 
-
-    if(oldMarker) {
+    if (oldMarker) {
       cmMap.removeLayer(layers[oldMarker]);
     }
-    
+
     setCMMap(null);
   }
-  
+
   // WMSTileLayer does not update itself in response to updates made to the
   // "layer" parameter, but does update in responds to changes made to
   // parameters in the "params" objecf. Therefore, we need anything that
-  // might change over the course of a user session to be in this 
+  // might change over the course of a user session to be in this
   // object.
-  let wmsParams = dataset ? {
-    layers: `x${dataset.file}/${dataset.variable}`,
-    time: dataset.time,
-    styles: dataset.styles,
-    logscale: dataset.logscale,
-    }: {};
+  let wmsParams = dataset
+    ? {
+        layers: `x${dataset.file}/${dataset.variable}`,
+        time: dataset.time,
+        styles: dataset.styles,
+        logscale: dataset.logscale,
+      }
+    : {};
 
-
-    //check to see if we have expected minmax available for this indicator
-    if(dataset && indicatorOptions?.[dataset.variable]) {
-        wmsParams["colorscalerange"] =
-          `${indicatorOptions[dataset.variable].minimum},${indicatorOptions[dataset.variable].maximum}`;
-    }
-    else if (dataset) {
-        console.log(`WARNING: missing configuration data for indicator ${dataset.variable}`);
-    }
+  //check to see if we have expected minmax available for this indicator
+  if (dataset && indicatorOptions?.[dataset.variable]) {
+    wmsParams["colorscalerange"] =
+      `${indicatorOptions[dataset.variable].minimum},${indicatorOptions[dataset.variable].maximum}`;
+  } else if (dataset) {
+    console.log(
+      `WARNING: missing configuration data for indicator ${dataset.variable}`,
+    );
+  }
 
   return (
     <div className="DataMap">
-        <BCBaseMap
-          id={"map"}
-          zoom={viewport.zoom}
-          center={viewport.center}
-          minZoom={4}
-          maxBounds={mapBounds}
-          maxBoundsViscosity={1.0}
-        >
-          <EEZLayer url={process.env.REACT_APP_EEZ_GEOJSON_URL} />
-          <SetView view={viewport}/>
-          <SimpleGeoJSON data={boundaryFeature} fill={false} color="#000000"/>
-          <SimpleGeoJSON data={downstreamFeature} fill={false} color="#6699FF"/>
-          <FeatureGroup>
-            <EditControl 
-              onCreated={handleAreaCreated}
-              onDeleted={handleAreaDeleted}
-              draw={{
-                  polyline: false,
-                  polygon: false,
-                  rectangle: false,
-                  circle: false,
-                  circlemarker: true,
-                  marker: false
-              }}
-              edit={{edit: false}}
-            />
-          </FeatureGroup>
-          {dataset &&
+      <BCBaseMap
+        id={"map"}
+        zoom={viewport.zoom}
+        center={viewport.center}
+        minZoom={5}
+        maxBounds={mapBounds}
+        maxBoundsViscosity={0.2}
+      >
+        <EEZLayer url={process.env.REACT_APP_EEZ_GEOJSON_URL} />
+        <SetView view={viewport} />
+        <SimpleGeoJSON data={boundaryFeature} fill={false} color="#000000" />
+        <SimpleGeoJSON data={downstreamFeature} fill={false} color="#6699FF" />
+        <FeatureGroup>
+          <EditControl
+            onCreated={handleAreaCreated}
+            onDeleted={handleAreaDeleted}
+            draw={{
+              polyline: false,
+              polygon: false,
+              rectangle: false,
+              circle: false,
+              circlemarker: true,
+              marker: false,
+            }}
+            edit={{ edit: false }}
+          />
+        </FeatureGroup>
+        {dataset && (
           <WMSTileLayer
             url={process.env.REACT_APP_NCWMS_URL}
-            format={'image/png'}
+            format={"image/png"}
             minZoom={4}
             noWrap={true}
             transparent={true}
-            version={'1.1.1'}
+            version={"1.1.1"}
             opacity={dataset.opacity}
             params={wmsParams}
-
             bounds={[
               [48, -139],
-              [61, -114]
+              [61, -114],
             ]}
           />
-          }
-        </BCBaseMap>
+        )}
+      </BCBaseMap>
     </div>
   );
 }
